68ac70ff0f9ff7b2b52861c70213002b3670e712
fractalfern <fractalfern123@gmail.com>
2026-05-11 20:44:06 -0500

    no magic strings

--- top-down-tutorial/scenes/player/player.gd
+++ top-down-tutorial/scenes/player/player.gd
@@ -1,17 +1,24 @@
 extends CharacterBody2D
 
+const ANIMATION_IDLE: String = "parameters/idle/blend_position"
+const ANIMATION_MOVE: String = "parameters/move/blend_position"
+
 @onready var input: InputComponent = %InputComponent
 @onready var movement: MovementComponent = %MovementComponent
 @onready var animation_tree: AnimationTree = $AnimationTree
 @onready var playback: AnimationNodeStateMachinePlayback = animation_tree.get("parameters/playback")
+
+func _ready() -> void:
+	assert(animation_tree.get(ANIMATION_IDLE) != null)
+	assert(animation_tree.get(ANIMATION_MOVE) != null)
 
 func _physics_process(delta: float) -> void:
 	input.update()
 	movement.direction = input.vector
 	
 	if movement.direction != Vector2.ZERO:
-		animation_tree.set("parameters/idle/blend_position", movement.direction)
-		animation_tree.set("parameters/move/blend_position", movement.direction)
+		animation_tree.set(ANIMATION_IDLE, movement.direction)
+		animation_tree.set(ANIMATION_MOVE, movement.direction)
 	
 	movement.tick(delta)
 
--- top-down-tutorial/scripts/movement_component.gd
+++ top-down-tutorial/scripts/movement_component.gd
@@ -2,11 +2,10 @@ class_name MovementComponent extends Node
 
 @export var body: CharacterBody2D
 @export var speed: float = 300.0
-@export var model: Sprite2D
 
 var direction: Vector2 = Vector2.ZERO
 
-func tick(delta: float) -> void:
+func tick(_delta: float) -> void:
 	assert(body != null)
 	
 	body.velocity = direction * speed
