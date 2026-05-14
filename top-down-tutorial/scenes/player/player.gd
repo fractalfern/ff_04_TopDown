bce25b5b9087b7093759f8b5ab71b21f9c77f9d0
fractalfern <fractalfern123@gmail.com>
2026-05-14 16:05:15 -0500

    removed components
    wait until duplication

--- top-down-tutorial/scenes/player/player.gd
+++ top-down-tutorial/scenes/player/player.gd
@@ -1,20 +1,32 @@
 extends CharacterBody2D
 
-@onready var movement: MovementComponent = %MovementComponent
-@onready var animation_tree: AnimationTree = $AnimationTree
+const ANIMATION_IDLE: String = "parameters/idle/blend_position"
+const ANIMATION_MOVE: String = "parameters/move/blend_position"
+const ANIMATION_ATTACK: String = "parameters/attack/blend_position"
 
-var input_vector: Vector2 = Vector2.ZERO
+@onready var animation_tree: AnimationTree = $AnimationTree
+@export var speed: float = 300.0
+
+var direction: Vector2 = Vector2.ZERO
+
+enum State {IDLE, MOVE, ATTACK}
+var state: State = State.IDLE
 
 func _physics_process(delta: float) -> void:
-	input_vector = Input.get_vector("move_left", "move_right", 
-							 "move_up", "move_down")
+	direction = Input.get_vector("move_left", "move_right", 
+							 	   "move_up", "move_down")
 
 	if Input.is_action_just_pressed("attack"):
 		attack()
 		
-	movement.direction = input_vector
-	movement.animate(animation_tree)
-	movement.move(self)
+	if direction != Vector2.ZERO:
+		animation_tree.set(ANIMATION_IDLE, direction)
+		animation_tree.set(ANIMATION_MOVE, direction)
+		animation_tree.set(ANIMATION_ATTACK, direction)
+	
+	self.velocity = direction * speed
+	self.move_and_slide()
 	
 func attack() -> void:
+	state = State.ATTACK
 	print("Is attacking")
